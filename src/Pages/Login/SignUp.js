6b93e656d85f50d97bfa95543105aa91871805da
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2023-04-18 14:01:43 +0600

    sign up UI added

---
+++ src/Pages/Login/SignUp.js
@@ -0,0 +1,91 @@
+import React from "react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+
+const SignUp = () => {
+  const { register, handleSubmit } = useForm();
+  const [data, setData] = useState("");
+
+  const handleSignUp = (data) => {
+    console.log(data);
+    // setLoginError('');
+    // signIn(data.email, data.password)
+    //     .then(result => {
+    //         const user = result.user;
+    //         console.log(user);
+    //         navigate(from, {replace: true});
+    //     })
+    //     .catch(error => {
+    //         console.log(error.message)
+    //         setLoginError(error.message);
+    //     });
+  };
+
+  return (
+    <div className="h-[800px] flex justify-center items-center ">
+      <div className="w-96 p-7 card  shadow-xl">
+        <h2 className="text-xl text-center">Sign Up</h2>
+        <form onSubmit={handleSubmit(handleSignUp)}>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              {" "}
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="text"
+              {...register("email", {
+                required: "Email Address is required",
+              })}
+              className="input input-bordered w-full max-w-xs"
+            />
+            {/* {errors.email && <p className='text-red-600'>{errors.email?.message}</p>} */}
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              {" "}
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 6,
+                  message: "Password must be 6 characters or longer",
+                },
+              })}
+              className="input input-bordered w-full max-w-xs"
+            />
+            <label className="label">
+              {" "}
+              <span className="label-text">Forget Password?</span>
+            </label>
+            {/* {errors.password && <p className='text-red-600'>{errors.password?.message}</p>} */}
+          </div>
+          <input
+            className="btn btn-primary w-full"
+            value="Sign Up"
+            type="submit"
+          />
+          <div>
+            {/* {loginError && <p className='text-red-600'>{loginError}</p>} */}
+          </div>
+        </form>
+        <p className="text-base my-2 text-center">
+          Already Signed In?
+          <Link className="text-primary" to="/login">
+            Please Login
+          </Link>
+        </p>
+        <div className="divider">OR</div>
+        <button className="btn btn-outline hover:btn-primary w-full">
+          <FcGoogle className="mr-5 text-2xl"></FcGoogle> CONTINUE WITH GOOGLE
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
