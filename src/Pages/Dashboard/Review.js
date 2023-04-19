fb7e70c7d47f02f3676bd075939790610fe3072f
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2023-04-19 20:06:33 +0600

    review ui added

--- src/Pages/Dashboard/Review.js
+++ src/Pages/Dashboard/Review.js
@@ -2,9 +2,37 @@ import React from "react";
 
 const Review = () => {
   return (
-    <div>
-      <h3>This is review component</h3>
-    </div>
+    <section>
+      <h3 className="text-3xl font-medium m-3">Review</h3>
+
+      <div className="m-4  shadow-lg w-full md:w-1/2 p-7">
+        <form onSubmit="">
+          <input
+            type="text"
+            placeholder="Your Name"
+            className="input input-bordered input-secondary w-full mb-3"
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Company Name, Designation"
+            className="input input-bordered input-secondary w-full mb-3"
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Ratings (1-5)"
+            className="input input-bordered input-secondary  w-full mb-3"
+          />
+          <br />
+          <textarea
+            placeholder="Description (up to 50 words)"
+            className="textarea textarea-bordered textarea-secondary textarea-lg  w-full mb-3"
+          ></textarea>
+          <input type="submit" className="btn btn-primary" value="Submit" />
+        </form>
+      </div>
+    </section>
   );
 };
 
